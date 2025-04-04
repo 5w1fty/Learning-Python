944664a1f8d9b613bd21ef18833f8bd45eab945d
Sw1fty <julius.pfleging@gmail.com>
2025-04-04 19:42:30 +0200

    Update password checker.py
    finaly functioning

--- password checker.py
+++ password checker.py
@@ -1,15 +1,47 @@
-def passwordchecker(password):
-    print("dein Password ist :" + password)
-    passwordIn = input("Gebe dein Password ein:")
-    if(passwordIn == password):
-        print("Richtiges Password!")
-        option = input("Options: 1=change password, 2=return")
-        if(option == 1):
-            password = input("New Password:")
-        if(option == 2):
-            return
-
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    if(b == 0):
+        print("Division by 0 not allowed!")
+    return a/b
+def calculator():
+    print("Welcome to my first inaccurate calculator!")
+    print("Options: 1=Addition, 2=Subtraction, 3=Multiplication, 4=Division")
+    option = int(input("Please enter your operator:"))
+    if option == 1:
+        try:
+            num1 = float(input("Enter num1:"))
+            num2 = float(input("Enter num2:"))
+            print(add(num1, num2))
+        except ValueError:
+            print("Error adding both numbers")
+    if option == 2:
+        try:
+            num1 = float(input("Enter num1:"))
+            num2 = float(input("Enter num2:"))
+            print(sub(num1, num2))
+        except ValueError:
+            print("Error subtracting both numbers")
+    if option == 3:
+        try:
+            num1 = float(input("Enter num1:"))
+            num2 = float(input("Enter num2:"))
+            print(mul(num1, num2))
+        except ValueError:
+            print("Error multiplicating both numbers")
+    if option == 4:
+        try:
+            num1 = float(input("Enter num1:"))
+            num2 = float(input("Enter num2:"))
+            print(div(num1, num2))
+        except ZeroDivisionError:
+            print("Division trough zero prohibitat")
+        except ValueError:
+            print("Error dividing both numbers.")
 if __name__ == "__main__":
-    pword = input("Definiere dein Password:")
     while(True):
-        passwordchecker(pword)
+        calculator()
